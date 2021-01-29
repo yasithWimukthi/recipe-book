e4d94031a07932c78ac1ad283ddf9d7e5112f41e
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-29 20:50:03 +0530

    Implement recipe detail component

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService{
 
@@ -7,8 +8,22 @@ export class RecipeService{
 
   // tslint:disable-next-line:variable-name
   private _recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'test descpition', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'),
-    new Recipe('Test Recipe-2', 'test descpition', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563')
+    new Recipe(
+      'Test Recipe',
+      'test descpition',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563',
+      [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Test Recipe-2',
+      'test descpition',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563',
+      [
+          new Ingredient('Meat', 1),
+          new Ingredient('Buns', 2)
+      ])
   ];
 
   getRecipes(): Recipe[] {
