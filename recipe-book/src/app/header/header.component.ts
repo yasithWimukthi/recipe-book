c2e7a19860bf069c037acf6a6f6c7f6c8a542b4d
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-03 09:15:18 +0530

    Implement router links

--- recipe-book/src/app/header/header.component.ts
+++ recipe-book/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 
 @Component({
@@ -8,10 +8,4 @@ import {Component, EventEmitter, Output} from '@angular/core';
 
 export class HeaderComponent{
 
-  @Output() featureSelected = new EventEmitter<string>();
-
-  // tslint:disable-next-line:typedef
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
