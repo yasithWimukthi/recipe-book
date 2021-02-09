3176260b9b052f5133bf8fd3c7504d83e5b159b0
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-09 18:51:54 +0530

    Implement recipe component

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -60,4 +60,14 @@ export class RecipeEditComponent implements OnInit {
   onSubmit() {
     console.log(this.recipeForm);
   }
+
+  onAddIngredient() {
+    // @ts-ignore
+    (<FormGroup>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name' : new FormControl(),
+        'amount' : new FormControl()
+      })
+    );
+  }
 }
