5dfd54c9b83427705dda36b592441e7e367e3d5b
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-09 17:36:42 +0530

    Implement recipe component

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {FormControl, FormGroup} from "@angular/forms";
 
+import {RecipeService} from "../recipe.service";
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
@@ -10,15 +12,39 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode: boolean;
+  recipeForm:FormGroup
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
       this.editMode = params.id != null;
-      console.log(this.editMode);
+      this.initForm();
     });
   }
 
+  private initForm(){
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if(this.editMode){
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name' : new FormControl(recipeName),
+      'imagePath' : new FormControl(recipeImagePath),
+      'description' : new FormControl(recipeDescription)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 }
