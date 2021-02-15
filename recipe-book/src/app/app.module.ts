526965c2e0793c63436417de1dccb47fc5c80770
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-15 10:48:13 +0530

    Store data in firebase

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -18,6 +19,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import {RecipeService} from "./recipes/recipe.service";
 
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +38,8 @@ import {RecipeService} from "./recipes/recipe.service";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [ShoppingListService,RecipeService],
   bootstrap: [AppComponent]
