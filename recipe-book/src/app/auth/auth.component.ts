843c0468ff392132a2ffce5027c81ed574d58ba6
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-18 10:59:15 +0530

    Sending the Signup Request

--- recipe-book/src/app/auth/auth.component.ts
+++ recipe-book/src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
 
+import {AuthService} from "./auth.service";
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -10,7 +12,7 @@ export class AuthComponent implements OnInit {
 
   isLoginMode = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +22,26 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(authForm: NgForm) {
+    if(!authForm.valid){
+      return;
+    }
 
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    if(this.isLoginMode){
+
+    }else{
+      this.authService.signup(email, password).subscribe(
+        resData =>{
+          console.log(resData)
+        },
+        error =>{
+          console.log(error)
+        }
+      );
+    }
+
+    authForm.reset();
   }
 }
