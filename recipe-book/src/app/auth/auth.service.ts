90c624c4acd8c0fbaa78582ca3107e7a30c56e62
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-20 21:33:14 +0530

    Handle login errors.

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 
@@ -28,26 +28,7 @@ export class AuthService{
         password,
         returnSecureToken  :true
       }
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred!'
-
-      if(!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMessage);
-      }
-
-      switch (errorRes.error.error) {
-        case 'EMAIL_EXISTS' :
-          errorMessage = 'The email address is already in use by another account.';
-          break;
-        case 'OPERATION_NOT_ALLOWED' :
-          errorMessage = 'Password sign-in is disabled for this project';
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER' :
-          errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-          break;
-      }
-      return throwError(errorMessage);
-    }));
+    ).pipe(catchError(this.errorHandle));
   }
 
   login(email:string, password:string){
@@ -58,6 +39,33 @@ export class AuthService{
         password,
         returnSecureToken  :true
       }
-    );
+    ).pipe(catchError(this.errorHandle));
+  }
+
+  private errorHandle(errorRes: HttpErrorResponse){
+    let errorMessage = 'An unknown error occurred!'
+
+    if(!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorRes.error.error) {
+      case 'EMAIL_EXISTS' :
+        errorMessage = 'The email address is already in use by another account.';
+        break;
+      case 'OPERATION_NOT_ALLOWED' :
+        errorMessage = 'Password sign-in is disabled for this project';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER' :
+        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
