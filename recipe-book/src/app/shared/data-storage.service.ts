3b616a59befee257fbb7c6fdf0da0e2f7480b569
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-26 16:29:04 +0530

    Attaching the Token with an Interceptor

--- recipe-book/src/app/shared/data-storage.service.ts
+++ recipe-book/src/app/shared/data-storage.service.ts
@@ -6,49 +6,43 @@ import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
 import {AuthService} from "../auth/auth.service";
 
-@Injectable({providedIn:'root'})
-export class DataStorageService{
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
   constructor(
-    private http:HttpClient,
-    private recipeService:RecipeService,
+    private http: HttpClient,
+    private recipeService: RecipeService,
     private authService: AuthService
   ) {
   }
 
-  storeRecipes(){
+  storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http.put(
       'https://recipe-book-e79d9-default-rtdb.firebaseio.com/recipe.json',
       recipes
-    ).subscribe(responce =>{
+    ).subscribe(responce => {
       console.log(responce)
     })
   }
 
-  fetchRecipes(){
+  fetchRecipes() {
 
-    return this.authService.user.pipe(take(1),exhaustMap(
-      user =>{
-        return this.http.get<Recipe[]>(
-          'https://recipe-book-e79d9-default-rtdb.firebaseio.com/recipe.json',
-          {
-            params: new HttpParams().set('auth',user.token)
-          }
-        )
-      }
-    ),
-        map(recipe=>{
-          return recipe.map(recipe =>{
+    return this.http.get<Recipe[]>(
+      'https://recipe-book-e79d9-default-rtdb.firebaseio.com/recipe.json'
+    )
+      .pipe(
+        map(recipe => {
+          return recipe.map(recipe => {
             return {
               ...recipe,
-              ingredients : recipe.ingredients ? recipe.ingredients : []
+              ingredients: recipe.ingredients ? recipe.ingredients : []
             }
           })
         }),
-        tap(recipe =>{
+        tap(recipe => {
           this.recipeService.setRecipes(recipe);
         })
-    )
+      )
 
   }
 }
