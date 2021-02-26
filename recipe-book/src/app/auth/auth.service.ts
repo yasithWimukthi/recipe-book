426fb020a6d3ee18d022bb6b1e0811d08f880b80
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-26 18:31:47 +0530

    Implement Auto Login

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -68,6 +68,30 @@ export class AuthService{
       );
   }
 
+  autoLogin(){
+    const userData: {
+      email:string,
+      id:string,
+      _token: string,
+      _tokenExpirationDate: string
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if(!userData){
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if(loadedUser.token){
+      this.user.next(loadedUser);
+    }
+  }
+
   logout(){
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -82,6 +106,7 @@ export class AuthService{
       expirationDate
     );
     this.user.next(user);
+    localStorage.setItem('userData',JSON.stringify(user));
   }
 
   private errorHandle(errorRes: HttpErrorResponse){
